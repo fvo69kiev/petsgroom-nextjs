8386bc57540bba8cdaff0b32da5f46afd7c5d657
Viktor <fvo69kiev@gmail.com>
2022-07-09 22:32:28 +0300

    Completed-GA

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
-// import { useEffect } from 'react';
-// import Router from 'next/router';
-// import { initGA, logPageView } from 'analytics';
+import { useEffect } from 'react';
+import Router from 'next/router';
+import { initGA, logPageView } from '../analytics/index';
 import 'react-multi-carousel/lib/styles.css';
 import 'react-modal-video/css/modal-video.min.css';
 import 'rc-drawer/assets/index.css';
@@ -9,11 +9,11 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import 'typeface-dm-sans';
 
 export default function CustomApp({ Component, pageProps }) {
-//   useEffect(() => {
-//     initGA();
-//     logPageView();
-//     Router.events.on('routeChangeComplete', logPageView);
-//   }, []);
+  useEffect(() => {
+    initGA();
+    logPageView();
+    Router.events.on('routeChangeComplete', logPageView);
+  }, []);
 
   return <Component {...pageProps} />;
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,7 +8,7 @@ class CustomDocument extends Document {
 
   render() {
     return (
-      <Html lang="en-US">
+      <Html lang="uk-UK">
         <Head />
         <body>
           <Main />
